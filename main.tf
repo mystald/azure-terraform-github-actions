ef636ebe4127a8b5ba8e33480a13c634e99fd16d
Ghozy Nurcahyadi <m.ghozy@ai.astra.co.id>
2023-11-29 03:17:26 +0700

    feat: change url

--- main.tf
+++ main.tf
@@ -55,8 +55,8 @@ resource "azurerm_linux_web_app" "webapp" {
 #  Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id                 = azurerm_linux_web_app.webapp.id
-  repo_url               = "https://github.com/Azure-Samples/python-docs-hello-world"
-  branch                 = "master"
+  repo_url               = "https://github.com/Azure-Samples/msdocs-python-flask-webapp-quickstart"
+  branch                 = "main"
   use_manual_integration = true
   use_mercurial          = false
 }
