13983c26122960e264959b4ae9aa522a4c023c10
Ghozy Nurcahyadi <m.ghozy@ai.astra.co.id>
2023-11-29 02:37:01 +0700

    feat: change using container

--- main.tf
+++ main.tf
@@ -50,10 +50,10 @@ resource "azurerm_linux_web_app" "webapp" {
 
 #  Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id                 = azurerm_linux_web_app.webapp.id
+  app_id = azurerm_linux_web_app.webapp.id
   github_action_configuration {
     container_configuration {
-      image_name = "azuredocs/aci-helloworld"
+      image_name   = "azuredocs/aci-helloworld"
       registry_url = "mcr.microsoft.com"
     }
   }
