e840ea4303d6ec7035a72729a306650ba52a056a
Ghozy Nurcahyadi <m.ghozy@ai.astra.co.id>
2023-11-29 03:33:00 +0700

    feat: using net sample

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "azurerm_linux_web_app" "webapp" {
     minimum_tls_version = "1.2"
     application_stack {
       docker_registry_url = "https://mcr.microsoft.com"
-      docker_image_name   = "azure-app-service/samples/aspnethelloworld:latest"
+      docker_image_name   = "dotnet/samples:latest"
     }
   }
 
