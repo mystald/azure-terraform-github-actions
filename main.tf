ae0c57b52cc6a4269171226dbe494b8fa0dd0777
Ghozy Nurcahyadi <m.ghozy@ai.astra.co.id>
2023-11-29 03:39:40 +0700

    feat: using alpine image

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "azurerm_linux_web_app" "webapp" {
     minimum_tls_version = "1.2"
     application_stack {
       docker_registry_url = "https://mcr.microsoft.com"
-      docker_image_name   = "dotnet/samples:latest"
+      docker_image_name   = "dotnet/samples:dotnetapp-alpine-amd64"
     }
   }
 
