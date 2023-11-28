fbf188dc6a2a070b1bd2f287f9682aa5a883d849
Ghozy Nurcahyadi <m.ghozy@ai.astra.co.id>
2023-11-29 02:32:19 +0700

    feat: change config container

--- main.tf
+++ main.tf
@@ -51,13 +51,10 @@ resource "azurerm_linux_web_app" "webapp" {
 #  Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id                 = azurerm_linux_web_app.webapp.id
-  repo_url               = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-  branch                 = "master"
-  use_manual_integration = true
   github_action_configuration {
-    code_configuration {
-      runtime_stack = "node"
-      runtime_version = "20"
+    container_configuration {
+      image_name = "azuredocs/aci-helloworld"
+      registry_url = "mcr.microsoft.com"
     }
   }
 }
