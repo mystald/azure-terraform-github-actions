4071345a1f9033022b961b0ee99d276832d79686
Ghozy Nurcahyadi <m.ghozy@ai.astra.co.id>
2023-11-29 03:26:35 +0700

    feat: deploy using container

--- main.tf
+++ main.tf
@@ -46,17 +46,18 @@ resource "azurerm_linux_web_app" "webapp" {
   site_config {
     minimum_tls_version = "1.2"
     application_stack {
-      python_version = "3.9"
+      docker_registry_url = "https://mcr.microsoft.com"
+      docker_image_name   = "azure-app-service/samples/aspnethelloworld:latest"
     }
   }
 
 }
 
-#  Deploy code from a public GitHub repo
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id                 = azurerm_linux_web_app.webapp.id
-  repo_url               = "https://github.com/Azure-Samples/msdocs-python-flask-webapp-quickstart"
-  branch                 = "main"
-  use_manual_integration = true
-  use_mercurial          = false
-}
+# #  Deploy code from a public GitHub repo
+# resource "azurerm_app_service_source_control" "sourcecontrol" {
+#   app_id                 = azurerm_linux_web_app.webapp.id
+#   repo_url               = "https://github.com/Azure-Samples/msdocs-python-flask-webapp-quickstart"
+#   branch                 = "main"
+#   use_manual_integration = true
+#   use_mercurial          = false
+# }
