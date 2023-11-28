9dbcdb030e29aaa1cd87606c2e8181568f6613b7
Ghozy Nurcahyadi <m.ghozy@ai.astra.co.id>
2023-11-29 02:44:56 +0700

    feat: using python sample

--- main.tf
+++ main.tf
@@ -50,14 +50,7 @@ resource "azurerm_linux_web_app" "webapp" {
 
 #  Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id                 = azurerm_linux_web_app.webapp.id
-  repo_url               = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-  branch                 = "master"
-  use_manual_integration = true
-  github_action_configuration {
-    code_configuration {
-      runtime_stack   = "node"
-      runtime_version = 20
-    }
-  }
+  app_id   = azurerm_linux_web_app.webapp.id
+  repo_url = "https://github.com/Azure-Samples/python-docs-hello-world"
+  branch   = "master"
 }
