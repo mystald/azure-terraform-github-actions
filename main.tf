bd9f26d82c90917fd5cdea26a1f62ac377daaed9
Hilman Abdan <hilman.abdan@ai.astra.co.id>
2023-11-29 11:53:19 +0700

    edit

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "tfgithubdemo"
+    resource_group_name  = "terraform-latihan"
+    storage_account_name = "terraformvyenald"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
@@ -29,7 +29,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-asp-${var.prefix_environment}"
+  name                = "webapp-serviceplan-vyenald-${var.prefix_environment}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
@@ -38,7 +38,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
-  name                = "webapp-${var.prefix_environment}"
+  name                = "webapp-vyenald-${var.prefix_environment}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.appserviceplan.id
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
 # Sample values
-resource_group_name = "rg-terraform-appservices"
+resource_group_name = "terraform-latihan"
 location            = "southeastasia"
 prefix_environment  = "prd"
