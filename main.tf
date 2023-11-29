e1e1c8348451bd4a450edcf980c7954134e1c7c3
Hilman Abdan <hilman.abdan@ai.astra.co.id>
2023-11-29 11:57:17 +0700

    testedit

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-serviceplan-vyenald-${var.prefix_environment}"
+  name                = "webapp-service-plan-vyenald-${var.prefix_environment}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
